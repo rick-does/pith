52422821324cce7939ad01416a0a88f171244581
Rick Mallery <richard_mallery@yahoo.com>
2026-05-22 12:45:27 -0600

    changing tree.yaml to mkdocs.yml

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,18 @@
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
+
+let sidebar = []
+try { sidebar = require('./sidebar.json') } catch {}
+
+export default {
+  title: 'PiTH',
+  description: 'A visual markdown workspace with hierarchy editing and prose analysis',
+  base: '/pith/',
+  themeConfig: {
+    search: { provider: 'local' },
+    sidebar,
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/rick-does/pith' }
+    ]
+  }
+}
