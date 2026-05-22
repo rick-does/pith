440d5dfe0f0b84a417c670cb59f0575fd06015de
Rick Mallery <richard_mallery@yahoo.com>
2026-05-22 12:58:03 -0600

    changing to vitapress

---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,41 @@
+import DefaultTheme from 'vitepress/theme'
+import { h } from 'vue'
+import './style.css'
+
+const SearchIcon = {
+  render() {
+    return h('button', {
+      class: 'nav-search-icon',
+      'aria-label': 'Search',
+      onClick() {
+        document.dispatchEvent(
+          new KeyboardEvent('keydown', { key: 'k', ctrlKey: true, bubbles: true, cancelable: true })
+        )
+      }
+    }, [
+      h('svg', {
+        xmlns: 'http://www.w3.org/2000/svg',
+        width: '18',
+        height: '18',
+        viewBox: '0 0 24 24',
+        fill: 'none',
+        stroke: 'currentColor',
+        'stroke-width': '2',
+        'stroke-linecap': 'round',
+        'stroke-linejoin': 'round'
+      }, [
+        h('circle', { cx: '11', cy: '11', r: '8' }),
+        h('path', { d: 'm21 21-4.35-4.35' })
+      ])
+    ])
+  }
+}
+
+export default {
+  extends: DefaultTheme,
+  Layout() {
+    return h(DefaultTheme.Layout, null, {
+      'nav-bar-content-before': () => h(SearchIcon)
+    })
+  }
+}
